2aed3c9eb72be5ce030bc1b4579f069abe4b2316
Uzzal Biswas <149400155+ubwasi@users.noreply.github.com>
2025-12-03 17:56:50 +0600

    wow

---
+++ lib/Theme/Theme.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+class LightTheme{
+  static final backgroundColors = Color(0xFFFFFFFF);
+  static final primaryColors = Colors.deepPurple;
+  static final titleColors = Colors.black12;
+  static final subTitleColors = Colors.grey;
+}
